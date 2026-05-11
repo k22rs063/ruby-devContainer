92456b9811fda182c6096fd16ac8f5225c8a71dc
篠原怜葵 <k22rs063@st.kyusan-u.ac.jp>
2026-05-11 12:41:11 +0900

    変数、条件分岐、繰り返し

--- hello.rb
+++ hello.rb
@@ -1,3 +1,27 @@
 #! /usr/bin/env ruby
 
 puts "Hello, World!"
+a = 0
+s = "ruby"
+d = 3.14
+puts "a:"+a.to_s()
+puts s
+puts d
+if a>3
+  puts "Lurge"
+elsif a<=0
+  puts "Negative"
+else
+  puts "Little"
+end
+
+x=5
+while x>0
+  print "x"
+  x-=1
+end
+puts
+5.times do |i|
+  print i.to_s + ","
+end
+puts
---
+++ main.java
@@ -0,0 +1,33 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5;
+        String s = "ruby";
+        double d = 3.14;
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(d);
+        system.out.println(s.length());
+
+        if(a>5){
+            system.out.println("Lurge");
+        } else if(a<0){
+            system.out.println("Negative");
+        } else {
+            system.out.println("Little");
+        }
+
+        int x=5;
+        wile(x>0){
+            system.out.println("a");
+            x--;
+        }
+        system.out.println();
+
+        for (int i=0; i<5; i++){
+        system.out.println(i + "a");
+        }
+        system.out.println();
+    }
+   
+}
