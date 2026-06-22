fc84637ae13811ef0a4254dbe2aebfc5356ea2ab
篠原怜葵 <k22rs063@st.kyusan-u.ac.jp>
2026-06-22 11:41:29 +0900

    HTTP1.1対応

--- client1.rb
+++ client1.rb
@@ -3,23 +3,30 @@
 
 require 'socket'
 
-host ='www.is.kyusan-u.ac.jp'
+host = 'www.is.kyusan-u.ac.jp'
 port = 'http'
 path = '/~toshi/'
 
-sock = TCPSocket.new host, port
-sock.print "GET #{path} HTTP/1.0\r\n"
-sock.print "Host: #{host}\r\n"
-sock.print "\r\n"
+# sock = TCPSocket.new host, port
+# sock.print "GET #{path} HTTP/1.0\r\n"
+# sock.print "Host: #{host}\r\n"
+# sock.print "\r\n"
 
+# sock = TCPSocket.new host, port
 # cmd = 'GET ' + path + " HTTP/1.0\r\n\r\n"
 # pp cmd
 # sock.print cmd
 
+sock = TCPSocket.new host, port
+cmd = 'GET ' + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"
+pp cmd
+sock.print cmd
+
+
 
 is_body = false
 while line = sock.gets
-  # pp line
+  pp line
   puts line if is_body
   is_body = true if line == "\r\n"
 end
