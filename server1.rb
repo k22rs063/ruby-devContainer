5e931f117a838a292d7c7693c4d6b1ae0287ee84
篠原怜葵 <k22rs063@st.kyusan-u.ac.jp>
2026-07-06 12:32:41 +0900

    レスポンスにHTTPヘッダーを追加委
    コンテンツをHTML化

--- server1.rb
+++ server1.rb
@@ -4,21 +4,32 @@
 require 'socket'
 def server s
   cmd, path, ver = s.gets.split " "
+  #HTTP/1.0として正しく返答
+  #1行目はHTTP/1.0 200 OK
+  #2行目　content-type: text/html
+  #3行目　空行
+  #4行目　コンテンツ
+  #5行目　ソケットをクローズ
+  
   if path == "/"
+    s.print "HTTP/1.0 200 OK\r\n"
+    s.print "Content-Type: text/html\r\n"
+    s.print "\r\n"
     pp "INDEX"
     s.puts "index"
+    s.puts "<h1>index</h1>"
   else
     pp "OTHER"
     s.puts "other"
   end
   s.close
   
-  while line = s.gets
-    pp line
-    s.puts line
-    break if line == "\r\n"
-  end
-  s.close
+  # while line = s.gets
+  #   pp line
+  #   s.puts line
+  #   break if line == "\r\n"
+  # end
+  # s.close
 end
 
 gs = TCPServer.open 'http'
