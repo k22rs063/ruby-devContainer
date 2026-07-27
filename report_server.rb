e6c18f05dabcfa4f8cd5216b138e60cd1166e126
篠原怜葵 <k22rs063@st.kyusan-u.ac.jp>
2026-07-27 15:00:34 +0900

    授業のものにおみくじを追加（動くかはわからない）

--- report_server.rb
+++ report_server.rb
@@ -6,10 +6,38 @@ require 'socket'
 
 omikuji = ["大吉", "中吉", "小吉", "末吉", "凶"]
 
+def server s
+  cmd, path, ver = s.gets.split " "
+
+  if path == "/"
+    kuji = omikuji.sample
+    s.print "HTTP/1.0 200 OK\r\n"
+    s.print "Content-Type: text/html\r\n"
+    s.print "\r\n"
+    s.puts "<h1>おみくじ</h1>"
+    s.puts "<p>今日の運勢は#{kuji}です</p>"
+  elsif path == "/api/now"
+    s.print "HTTP/1.0 200 OK\r\n"
+    s.print "Content-Type: application/json\r\n"
+    s.print "\r\n"
+    s.puts "{"
+    s.puts '"\"time\:"' + "#{Time.now}" + '"'
+    s.puts "}"
+  else
+    s.print "HTTP/1.0 404 Not Found\r\n"
+    s.print "Content-Type: text/html\r\n"
+    s.print "\r\n"
+    s.puts "<h1>404 Not Found</h1>"
+    s.puts "<p>404 Not Found</p>"
+  end
+end
+
+
 gs = TCPServer.open 80
 loop do
   pp "start accept"
   s = gs.accept
-  s.puts omikuji.sample
-  s.close
+  Thread.new do
+    server s
+  end
 end
