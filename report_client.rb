f6892d4b09e2d83f4b163e771ce8ee541bff2419
篠原怜葵 <k22rs063@st.kyusan-u.ac.jp>
2026-07-30 04:17:33 +0900

    claient1.rbをもとにクライアントプログラム作成

--- report_client.rb
+++ report_client.rb
@@ -4,6 +4,19 @@
 
 require 'socket'
 
-s = TCPSocket.open('localhost', 80)
-puts s.gets
-s.close
+host = ARGV[0]
+port = 'http'
+path = ARGV[1]
+if path == nil
+    path = '/'
+end
+
+sock = TCPSocket.new host, port
+sock.print 'GET ' + path + " HTTP/1.0\r\n\r\n"
+
+is_body = false
+while line = sock.gets
+    pp line
+    puts line if is_body
+    is_body = true if line == "\r\n"
+end
