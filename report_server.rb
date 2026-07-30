21fcbe1f7501133710731b5a6e76ec114336f190
篠原怜葵 <k22rs063@st.kyusan-u.ac.jp>
2026-07-30 18:54:49 +0900

    パスを判定して404を返すように変更

--- report_server.rb
+++ report_server.rb
@@ -19,20 +19,19 @@ def server s
   elsif cmd == "GET"
     if path == "/"
       kuji = OMIKUJI.sample
-    s.print "HTTP/1.0 200 OK\r\n"
-    s.print "Content-Type: text/html; charset=UTF-8\r\n"
-    s.print "\r\n"
-    s.puts "<h1>おみくじ</h1>"
-    s.puts "<p>今日の運勢は#{kuji}です</p>"
-    
-    s.puts "<p>#{Time.now.getlocal('+09:00').strftime('%Y年%m月%d日 %H時%M分')}</p>"
+      s.print "HTTP/1.0 200 OK\r\n"
+      s.print "Content-Type: text/html; charset=UTF-8\r\n"
+      s.print "\r\n"
+      s.puts "<h1>おみくじ</h1>"
+      s.puts "<p>今日の運勢は#{kuji}です</p>"
+      s.puts "<p>#{Time.now.getlocal('+09:00').strftime('%Y年%m月%d日 %H時%M分')}</p>"
+    else
+      s.print "HTTP/1.0 404 Not Found\r\n"
+      s.print "Content-Type: text/html\r\n"
+      s.print "\r\n"
+      s.puts "<h1>404 Not Found</h1>"
+      s.puts "<p>404 Not Found</p>"
     end
-  else
-    s.print "HTTP/1.0 404 Not Found\r\n"
-    s.print "Content-Type: text/html\r\n"
-    s.print "\r\n"
-    s.puts "<h1>404 Not Found</h1>"
-    s.puts "<p>404 Not Found</p>"
   end
 end
 
